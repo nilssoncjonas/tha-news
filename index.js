29d486622cec3b37c1474066f94e6e72fb5da3ad
nilssoncjonas <nilssoncjonas@gmail.com>
2024-02-10 21:44:43 +0100

    Added logic for creating new json file every day

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import { svtLatestNews } from "./svt.js";
 import { readJSONFromFile } from "./utils/json.js";
 import cron from 'node-cron'; 
+import * as fs from 'fs';
 
 console.log('server started:', new Date().toLocaleString('sv-SE'));
 
 let count = 0;
 
-cron.schedule('*/30 * * * *', async () => {
+cron.schedule(' */31 * * * * ', async () => {
 
 	const date = new Date();
 	console.log('Scraping every 30 min', date.toLocaleString('sv-SE'), ++count, 'times');
 
-	await svtLatestNews()
+	await svtLatestNews(date.toLocaleDateString('sv-SE'))
 
-	const updated = readJSONFromFile('data/svt-latest.json')
-
+	const updated = readJSONFromFile(`data/svt/${date.toLocaleDateString('sv-SE')}.json`)
 	console.log(`Number of news: ${updated.length}`)
 	console.log('----------')
 }); 
+
+cron.schedule('0 0 * * *', async () => {
+	const date = new Date();
+	console.log('Created new json store file', date.toLocaleDateString('sv-SE'));
+	fs.writeFileSync(`data/svt/${date.toLocaleDateString('sv-SE')}.json`, JSON.stringify(data, null, 2));
+});
