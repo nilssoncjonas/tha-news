4249c642c4c10a2f61539efed8c3c91153aa8220
nilssoncjonas <nilssoncjonas@gmail.com>
2024-02-12 09:10:00 +0100

    Updated logic for when reading file, if th efile dosen't exist, it created, added simple error handling

--- utils/json.js
+++ utils/json.js
@@ -1,14 +1,28 @@
 import * as fs from 'fs';
 
 export const readJSONFromFile = (filePath) => {
+	if (!fs.existsSync(filePath)) {
+		try {
+			fs.writeFileSync(filePath, '[]', 'utf8');
+		} catch (error) {
+			console.log(`[ERROR]: creating file: ${filePath}`)
+		}
+	}
 	try {
 		const jsonData = fs.readFileSync(filePath);
 		return JSON.parse(jsonData);
 	} catch (error) {
+		console.log(`[ERROR]: reading file: ${filePath}`)
 		return [];
 	}
 };
 
 export const writeJSONToFile = (filePath, data) => {
-	fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+	try {
+		fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+	} catch (error) {
+		console.log(`[ERROR]: reading file: ${filePath}`)
+	}
 };
+
+// npm fs-extra ?
