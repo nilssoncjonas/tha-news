1df83ac665b9f33485594697fd3a515b5ac22352
nilssoncjonas <nilssoncjonas@gmail.com>
2024-05-15 06:28:53 +0200

    adjusted logic

--- svt.js
+++ svt.js
@@ -5,7 +5,7 @@ import { store } from './utils/store.js';
 const url = 'https://www.svt.se'
 
 export const svtLatestNews = async (date) => {
-	
+
 	const browser = await puppeteer.launch({ headless: true })
 	console.log('Getting data...')
 
@@ -17,11 +17,11 @@ export const svtLatestNews = async (date) => {
 
 	const data = await page.evaluate((url) => {
 
-		const arr = Array.from(document.querySelectorAll('.LatestNewsItem__root___oHHgk'))
+		const arr = Array.from(document.querySelectorAll('.LatestNewsItem__root___xKB7B'))
 		const data = arr.map((i) => ({
-			title: i.querySelector('.LatestNewsItem__title___GFcfW').innerText,
-			url: url + i.querySelector('.LatestNewsItem__link___ERU4W').getAttribute('href'),
-			category: i.querySelector('.LatestNewsItem__section___FRr4a').innerText,
+			title: i.querySelector('.LatestNewsItem__title___YAJH7').innerText,
+			url: url + i.querySelector('.LatestNewsItem__link___VDAfy').getAttribute('href'),
+			category: i.querySelector('.LatestNewsItem__section___o5a-Z').innerText,
 			published: i.querySelector('a span span time').getAttribute('datetime')
 		}))
 		return data
@@ -29,26 +29,30 @@ export const svtLatestNews = async (date) => {
 
 	await browser.close()
 	console.log('Data received...')
+	if (!data || data.length === 0) {
+		console.log('No data available')
+		return
+	}
 
-	console.log('Checking for items published today...') 
+	console.log('Checking for items published today...')
 	const filteredObjects = data.filter(object => {
-  const publishedDate = new Date(object.published).toLocaleDateString('sv-SE'); // Extract date from "published"
-  return publishedDate === date;
+		const publishedDate = new Date(object.published).toLocaleDateString('sv-SE'); // Extract date from "published"
+		return publishedDate === date;
 	});
-	
+
 	// Getting existing data to compare against the new
 	let existingData = readJSONFromFile(`data/svt/${date}.json`)
-	
+
 	console.log('Checking for new items...')
 	const uniqueNewData = filteredObjects.filter(newItem => !existingData.some((existingItem) => existingItem.url === newItem.url));
 
-	if (uniqueNewData.length > 0) {		
+	if (uniqueNewData.length > 0) {
 		console.log('New items found, storing...')
-		
-		await store(uniqueNewData)	
-		
+
+		await store(uniqueNewData)
+
 		const updatedData = [...uniqueNewData, ...existingData];
-		
+
 		writeJSONToFile(`data/svt/${date}.json`, updatedData)
 
 		console.log('finished, New items:', uniqueNewData.length)
