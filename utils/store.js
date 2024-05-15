241dec9503d2327fba2d5428ef02f267d00787a6
nilssoncjonas <nilssoncjonas@gmail.com>
2024-05-15 14:31:02 +0200

    Added logic to save a log if writing to supabase fails

--- utils/store.js
+++ utils/store.js
@@ -1,9 +1,19 @@
 import  supabase  from '../config/supabase.js';
 
 export const store = async (newData) => {
-	console.log('store to supabase')
+	// console.log('store to supabase')
 	const { data, error } = await supabase
 		.from('svt-latest-news')
 		.insert(newData)		
-	if (error) console.error('error', error);
+	if(error) {
+		console.error('error', error);
+		const date = new Date()
+		const file = `data/logs/supabase/errors.json`
+		const logs = readJSONFromFile(file)
+		const logItem = {
+			"time": `${date.toLocaleDateString('sv-SE')}-${date.toLocaleTimeString('sv-SE')}`,
+			"error": error
+		}
+		writeJSONToFile(file, [logItem, ...logs])
+	}
 }
